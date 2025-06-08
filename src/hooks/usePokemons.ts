5f52edce396f6a4ee7c265c6732f5ffd6ed7f796
ErickRz21 <erickuriel238@gmail.com>
2025-06-08 15:23:55 -0600

    Refactor usePokemons hook to streamline data fetching and improve error handling

--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -11,33 +11,38 @@ export function usePokemons(limit = 1000) {
       try {
         setLoading(true);
         setError(null);
-
+  
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
         if (!res.ok) throw new Error("Failed to fetch Pokémon list");
-
+  
         const data = await res.json();
-        const results = data.results;
-
+        console.log("Basic list:", data.results);
+  
         const detailedData = await Promise.all(
-          results.map(async (poke: { name: string; url: string }) => {
+          data.results.map(async (poke: { name: string; url: string }) => {
             const res = await fetch(poke.url);
             if (!res.ok) throw new Error(`Failed to fetch ${poke.name}`);
-            return await res.json();
+            const detail = await res.json();
+            console.log("Fetched detail:", detail.name);
+            return detail;
           }),
         );
-
+  
         setPokemons(detailedData);
-        } catch (err: unknown) {
+      } catch (err: unknown) {
         if (err instanceof Error) {
           setError(err.message);
         } else {
           setError(String(err));
         }
+      } finally {
+        setLoading(false);
       }
     };
-
+  
     fetchPokemons();
   }, [limit]);
+  
 
   return { pokemons, loading, error };
 }
